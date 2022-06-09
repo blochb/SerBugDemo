bce7f397919a31270a2cf4d57e0d3a9c9fc300b0
Benny Bloch <benny.bloch@rivulis.com>
2022-06-09 19:36:03 +0300

    Add project files.

---
+++ SerBug/test.c
@@ -0,0 +1,34 @@
+﻿#include <stdlib.h>
+
+double sum_double(double a, double b, double c) {
+	return a + b + c;
+}
+
+void* test_malloc_d3(double a, double b, double c) {
+	size_t sum = (int)(a + b + c);
+	return malloc(sum);
+}
+
+void* test_malloc_d2(double a, double b) {
+	size_t sum = (int)(a + b);
+	return malloc(sum);
+}
+
+void* test_malloc_d1(double a) {
+	size_t sum = (int)a;
+	return malloc(sum);
+}
+
+void* test_malloc_f3(float a, float b, float c) {
+	size_t sum = (int)(a + b + c);
+	return malloc(sum);
+}
+
+void* test_malloc_df2(double a, float b) {
+	size_t sum = (int)(a + b);
+	return malloc(sum);
+}
+
+void test_free(void* mem) {
+	free(mem);
+}
